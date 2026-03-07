6b37a5cae61b1dfed2ad731aa25181571c366f1f
AndGP0502 <andre.garzon@udla.edu.ec>
2026-03-07 00:14:26 -0500

    databse

--- database/database.py
+++ database/database.py
@@ -1,9 +1,9 @@
 import sqlite3
 
 conexion = sqlite3.connect("gym.db")
-
 cursor = conexion.cursor()
 
+# Tabla clientes
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS clientes(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,8 +13,55 @@ fecha_registro TEXT
 )
 """)
 
+# Tabla membresias
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS membresias(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+nombre_plan TEXT,
+precio REAL,
+duracion_dias INTEGER
+)
+""")
+
+# Tabla pagos
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS pagos(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+cliente_id INTEGER,
+monto REAL,
+fecha_pago TEXT
+)
+""")
+
+# Tabla de pagos parciales
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS suscripciones(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+cliente_id INTEGER,
+membresia_id INTEGER,
+fecha_inicio TEXT,
+fecha_vencimiento TEXT,
+precio_total REAL,
+pagado REAL,
+pendiente REAL
+)
+""")
+
+
+
+# Tabla suscripciones (para controlar membresías activas)
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS suscripciones(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+cliente_id INTEGER,
+membresia_id INTEGER,
+fecha_inicio TEXT,
+fecha_vencimiento TEXT
+)
+""")
+
 conexion.commit()
 
-print("Tabla clientes creada")
+print("Base de datos del gimnasio creada correctamente")
 
 conexion.close()
